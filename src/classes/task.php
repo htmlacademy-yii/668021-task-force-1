cc0a14694da40d0ff16218098341aec799c5f673
Serg Wulf <serg.wulf@mail.ru>
2019-10-13 12:28:52 +0300

    add code in function getNextStatus

--- src/classes/task.php
+++ src/classes/task.php
@@ -14,18 +14,43 @@ class Task {
     const ACTION_FINISH = "завершить";
 
     const ROLE_EXECUTOR = 'исполнитель';
-    const ROLE_COSTUMER = 'заказчик';
+    const ROLE_CUSTOMER = 'заказчик';
 
 
 
-    private $id_executor;
-    private $id_costumer;
-    private $active_status;
+    private $executor_id;
+    private $customer_id;
+    private $status;
     private $deadline;
 
     public function __construct()
     {
-        // в конструкторе нужно определить заказчика, а также сменить статус задачи на новая
+        // в конструкторе нужно определить заказчика, а также сменить статус  задачи на новая
+    }
+
+    public function getNextStatus ($action)
+    {
+        switch ($action) {
+            case self::ACTION_NEW:
+                $this->status = self::STATUS_NEW;
+                break;
+            case self::ACTION_FAIL:
+                $this->status = self::STATUS_FAILED;
+                break;
+            case self::ACTION_CANCEL:
+                $this->status = self::STATUS_CANCELED;
+                break;
+            case self::ACTION_START:
+                $this->status = self::STATUS_PERFORMING;
+                break;
+            case self::ACTION_FINISH:
+                $this->status = self::STATUS_FINISHED;
+                break;
+            default:
+                $this->status = null;
+        }
+
+        return $this->status;
     }
 
     public function get_status()
@@ -39,3 +64,10 @@ class Task {
     }
 
 }
+
+
+$task = new Task();
+
+$nextStatus = $task->getNextStatus(Task::ACTION_FINISH);
+echo $nextStatus;
+
